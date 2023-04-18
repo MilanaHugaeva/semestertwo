4b5ff5c46a852c47ef8dcfda73558ac835a33ee6
Хугаева Милана Маратовна <yan.mili@yandex.ru>
2023-04-18 19:22:23 +0300

    больше файлов

--- parallelogram/parallelogram.h
+++ parallelogram/parallelogram.h
@@ -5,6 +5,22 @@ namespace miit::figure
 {
 	class parallelogram
 	{
+	public:
+		/// <summary>
+		/// Создает параллелограмм по трём точкам 
+		/// </summary>
+		/// <param name="vertex1">Вершина 1</param>
+		/// <param name="vertex2">Вершина 2</param>
+		/// <param name="vertex3">Вершина 3</param>
 		parallelogram(const point& vertex1, const point& vertex2, const point& vertex3);
+
+		const point& vertex1() const;
+		const point& vertex2() const;
+		const point& vertex3() const;
+
+	private:
+		const point& vertex1;
+		const point& vertex1;
+		const point& vertex1;
 	};
 }
