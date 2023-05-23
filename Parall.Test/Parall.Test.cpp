c9b521c9a94db0a85230828e3d59ca979fdc8f44
Хугаева Милана Маратовна <yan.mili@yandex.ru>
2023-05-23 17:03:00 +0300

    Доделали тесты

--- Parall.Test/Parall.Test.cpp
+++ Parall.Test/Parall.Test.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "CppUnitTest.h"
 #include "../helplib/math.h"
 #include "../parallelepiped/parallelepiped.h"
@@ -11,25 +10,28 @@ namespace ParallTest
 	TEST_CLASS(ParallTest)
 	{
 	public:
-		
-		TEST_METHOD(GetAreaTestParall_ValidDataSuccess)
+		TEST_METHOD(GetArea_ValidData_Success)
 		{
-			point vertex1 = point(0, 0, 0);
-			point vertex2 = point(0, 10, 0);
-			point vertex3 = point(10, 10, 10);
-			parallelepiped paral_1 = parallelepiped(vertex1, vertex2, vertex3);
-			double exp_area = 346.41016151377551;
-			Assert::IsTrue(miit::help::math::is_equal_doubles(exp_area, paral_1.math_area()));
+			const auto vertex1 = point(0, 0, 0);
+			const auto vertex2 = point(0, 10, 0);
+			const auto vertex3 = point(10, 10, 10);
+			const auto paral_1 = parallelepiped(vertex1, vertex2, vertex3);
+			const auto exp_area = 346.41016151377551;
+			const auto actual = paral_1.math_area();
+
+			Assert::IsTrue(miit::help::math::is_equal_doubles(exp_area, actual));
 		}
 
-		TEST_METHOD(GetPerimetrTestParall_ValidDataSuccess)
+		TEST_METHOD(GetPerimeter_ValidData_Success)
 		{
-			point vertex1 = point(0, 0, 0);
-			point vertex2 = point(0, 10, 0);
-			point vertex3 = point(10, 10, 10);
-			parallelepiped paral_1 = parallelepiped(vertex1, vertex2, vertex3);
-			double exp_area = 54.641016151377549;
-			Assert::IsTrue(miit::help::math::is_equal_doubles(exp_area, paral_1.math_perimeter()));
+			const auto vertex1 = point(0, 0, 0);
+			const auto vertex2 = point(0, 10, 0);
+			const auto vertex3 = point(10, 10, 10);
+			const auto paral_1 = parallelepiped(vertex1, vertex2, vertex3);
+			const auto exp_area = 54.641016151377549;
+			const auto actual = paral_1.math_perimeter();
+
+			Assert::IsTrue(miit::help::math::is_equal_doubles(exp_area, actual));
 
 		}
 	};
